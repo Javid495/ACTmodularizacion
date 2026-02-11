b22086845c05fbfeceea0f5887ea7e6725d32d4e
jose gabriel roa <jroadiaz@gmail.com>
2026-02-11 18:14:41 -0500

    se realizo funciones de la actividad

---
+++ app.js
@@ -0,0 +1,39 @@
+
+
+
+// function listarIngredientes(ingredientes){
+//     console.log("alistar ingredientes : " + ingredientes);
+    
+// }
+
+// listarIngredientes("cafe");
+// listarIngredientes("agua");
+// listarIngredientes("azucar");
+// listarIngredientes("leche");
+
+
+// 1 gramo de café molido por cada 16-17 ml de agua. Para una taza estándar, 
+// se recomiendan aproximadamente 6 a 8 gramos de café 
+// por cada 120-150 ml de agua caliente
+
+function formulaCafeportaza (agua, cafe){
+    return "agregar "+ agua + "ml de agua y "+ cafe + " gramos de cafe";
+}
+
+let taza = formulaCafeportaza (6, 120);
+console.log(taza);
+
+
+// const prepararCafe = (agua , cafe) => `Agrega ${agua} ml de agua y ${cafe} gramos de café.`;
+
+// console.log(prepararCafe(120, 6));
+
+
+
+// function IngredienteExtra (extra){
+//     return "si, deseas puedes agregar" + extra + " opcionalmente." ;
+
+// }
+
+// console.log (IngredienteExtra(" leche"));
+// console.log (IngredienteExtra(" azucar"));
